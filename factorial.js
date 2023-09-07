f07915bef970bcc2f8ca375d143ceaa19f50c5c6
EnyeerManuel77 <139489344+EnyeerManuel77@users.noreply.github.com>
2023-09-07 08:51:37 -0500

    tercer codigo

---
+++ factorial.js
@@ -0,0 +1,11 @@
+//Realiza un algoritmo que calcule el factorial de un número.
+
+let x = parseInt(prompt("digite un numero: "));
+let cont = 1
+
+for (i==1; i >= x; i ++){
+    y = parseInt(prompt("veces que los vas a factorizar: "));
+    cont ++;
+    let z = x * y;
+    alert("elresultado es: " +z);
+}
