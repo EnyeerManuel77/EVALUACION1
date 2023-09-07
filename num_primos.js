2a2579c00db71e8b15de812ae5982b45778110cf
EnyeerManuel77 <139489344+EnyeerManuel77@users.noreply.github.com>
2023-09-07 09:03:04 -0500

    cuarto codigo

---
+++ num_primos.js
@@ -0,0 +1,9 @@
+// 4.	Realiza un algoritmo que muestre los números primos que hay entre 250 y 550
+
+let x = alert("Empieza en 250")
+let y = alert("Termina en 500")
+
+
+for (i==x; i >= x ;i++){
+    if(i % 2 && i === 0);
+} alert(i);
