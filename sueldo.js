a5c08815b33c58778a66e890c5fc2df47432a570
EnyeerManuel77 <139489344+EnyeerManuel77@users.noreply.github.com>
2023-09-07 08:29:13 -0500

    primer ejercicio

---
+++ sueldo.js
@@ -0,0 +1,8 @@
+let NumEmpleados = parseInt("digite el numero de empleados: ")
+let promedio = []
+let cont = 0
+
+for(i==1; i >= NumEmpleados; i++){
+    let x = prompt("Nombre del empleado: ");
+    let y = parseFloat("Sueldo del empleado: ");
+}
